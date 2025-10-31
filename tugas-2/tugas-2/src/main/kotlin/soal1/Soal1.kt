651b99fd30a8c29024708aec06e92e070b888d8a
Nasywa Kamila <153457446+NasywaKml@users.noreply.github.com>
2025-10-31 22:33:50 +0700

    summation list using insertion sort algorithm

--- tugas-2/tugas-2/src/main/kotlin/soal1/Soal1.kt
+++ tugas-2/tugas-2/src/main/kotlin/soal1/Soal1.kt
@@ -2,5 +2,17 @@ package soal1
 
 // TODO
 fun sumOfBigThree(numbers: List<Int>): Int {
-    return 0
+    val n = numbers.size
+    for (i in 1 until n) {
+    	val key = numbers[i]
+    	var j = i - 1 
+        while (j >= 0 && key > numbers[j]) {
+            numbers[j + 1] = numbers[j] 
+            j--
+        }
+    	numbers[j + 1] = key 
+    }
+
+    return numbers[0]+numbers[1]+numbers[2]
+
 }
