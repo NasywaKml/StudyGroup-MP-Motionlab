84e3cf4b9272cdbbe26e6f0568b9bd67dd9d57c9
Nasywa Kamila <153457446+NasywaKml@users.noreply.github.com>
2025-10-31 22:55:13 +0700

    getMiddleCharacters using substring
    Implement getMiddleCharacters function to return the middle character(s) of a string.

--- tugas-2/tugas-2/src/main/kotlin/soal3/Soal3.kt
+++ tugas-2/tugas-2/src/main/kotlin/soal3/Soal3.kt
@@ -2,5 +2,19 @@ package soal3
 
 // TODO
 fun getMiddleCharacters(string: String): String {
-    return ""
+    val size = string.length
+
+    if (size % 2 != 0) {
+        val middleIndex = size / 2
+        return( string[middleIndex].toString())
+        
+    }
+    else {
+        val n = size / 2
+        val char1 = string[n - 1] 
+        val char2 = string[n]
+        
+        return( char1.toString() + char2.toString())
+    }
+
 }
