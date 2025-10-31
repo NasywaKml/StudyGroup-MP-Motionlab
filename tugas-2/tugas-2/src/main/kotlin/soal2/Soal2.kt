cb6dee23be5f80f334e5c171f6d9100315308386
Nasywa Kamila <153457446+NasywaKml@users.noreply.github.com>
2025-10-31 22:47:21 +0700

    sum of max&min using sorting array
    Implement minAndMax function to calculate the sum of minimum and maximum digits of a number.

--- tugas-2/tugas-2/src/main/kotlin/soal2/Soal2.kt
+++ tugas-2/tugas-2/src/main/kotlin/soal2/Soal2.kt
@@ -2,5 +2,24 @@ package soal2
 
 // TODO
 fun minAndMax(number: Int): Int {
-    return 0
+   val numbers = mutableListOf<Int>()
+    while (num != 0) {
+        val digit = num % 10
+        numbers.add(digit) 
+        num /= 10 
+    }
+    val n = numbers.size
+    
+    for (i in 1 until n) {
+        val key = numbers[i]
+        var j = i - 1
+       
+        while (j >= 0 && key < numbers[j]) {
+            numbers[j + 1] = numbers[j]
+            j--
+        }
+        numbers[j + 1] = key
+    }
+    return (numbers[0] + numbers[numbers.size - 1])
+    
 }
